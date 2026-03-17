52cd36c6b6c018ca1ba340cee2b2ea30b40c07ea
jgommes1501 <jgommes1501@g.educaand.es>
2026-03-17 19:48:07 +0100

    update apiUrl to point to the correct Render.com service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Cambia esta URL por la URL de tu servicio en Render.com una vez desplegado
   // Ejemplo: 'https://proyect-peli-api.onrender.com'
-  apiUrl: 'https://TU-SERVICIO.onrender.com'
+  apiUrl: 'https://proyect-peli-api.onrender.com'
 };
